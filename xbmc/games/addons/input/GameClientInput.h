020710eeaf2c7662021f9eb606d89fc35fc5f84d
Garrett Brown <themagnificentmrb@gmail.com>
2018-07-24 12:41:16 -0700

    Game API v1.0.40: Add feature names to controller layouts
    This change expands the existing controller layout with vectors of
    names for the various types of features on each controller.
    
    The API has been modified to report controller layouts using a dedicated
    function, which is called after the input topology is loaded. Other API
    calls have been simplified to just pass controller ID instead of the full
    layout.

--- xbmc/games/addons/input/GameClientInput.h
+++ xbmc/games/addons/input/GameClientInput.h
@@ -31,6 +31,7 @@ namespace JOYSTICK
 namespace GAME
 {
   class CGameClient;
+  class CGameClientController;
   class CGameClientHardware;
   class CGameClientJoystick;
   class CGameClientKeyboard;
@@ -90,6 +91,7 @@ namespace GAME
 
     // Private input helpers
     void LoadTopology();
+    void SetControllerLayouts(const ControllerVector &controllers);
     void ProcessJoysticks();
     PortMap MapJoysticks(const PERIPHERALS::PeripheralVector &peripheralJoysticks,
                          const JoystickMap &gameClientjoysticks) const;
@@ -103,6 +105,8 @@ namespace GAME
 
     // Input properties
     std::unique_ptr<CGameClientTopology> m_topology;
+    using ControllerLayoutMap = std::map<std::string, std::unique_ptr<CGameClientController>>;
+    ControllerLayoutMap m_controllerLayouts;
     JoystickMap m_joysticks;
     PortMap m_portMap;
     std::unique_ptr<CGameClientKeyboard> m_keyboard;
