d5d1a679934390bf471e46120dfd14f7f16544e9
Nick Siakas <nikos.siakas@gmail.com>
2020-08-23 13:13:21 +0300

    [PR 120] RetroPlayer: Cheevos subsystem

--- xbmc/games/dialogs/DialogGameDefines.h
+++ xbmc/games/dialogs/DialogGameDefines.h
@@ -19,5 +19,8 @@
  */
 #pragma once
 
+// Name of list item property for savestate captions
+#define SAVESTATE_CAPTION "savestate.caption"
+
 // String of list item property "game.videofilter" when no filter is set
 #define PROPERTY_NO_VIDEO_FILTER ""
